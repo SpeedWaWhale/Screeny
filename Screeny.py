a153cdb0bda418826d1c6227405472eb59d649aa
JC LanYanFock <JC.LanYanFock@kratosdefense.com>
2022-04-21 18:17:57 +0200

    A

--- Screeny.py
+++ Screeny.py
@@ -6,8 +6,6 @@ from TP_lib import epd2in13_V2
 
 epd = epd2in13_V2.EPD_2IN13_V2()
 gt = gt1151.GT1151()
-GT_Dev = gt1151.GT_Development()
-GT_Old = gt1151.GT_Development()
 
 print("init and Clear")
 epd.init(epd.FULL_UPDATE)
