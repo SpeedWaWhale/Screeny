842a0012a28d234c1039296c500b53a0eb3f638f
JC LanYanFock <JC.LanYanFock@kratosdefense.com>
2022-04-21 18:16:05 +0200

    Add clear

--- Screeny.py
+++ Screeny.py
@@ -1,1 +1,15 @@
-print("hello")
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+from TP_lib import gt1151
+from TP_lib import epd2in13_V2
+
+epd = epd2in13_V2.EPD_2IN13_V2()
+gt = gt1151.GT1151()
+GT_Dev = gt1151.GT_Development()
+GT_Old = gt1151.GT_Development()
+
+logging.info("init and Clear")
+epd.init(epd.FULL_UPDATE)
+gt.GT_Init()
+epd.Clear(0xFF)
