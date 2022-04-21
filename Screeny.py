adf3360d65deecd1f56237ad0933755c5f5e9f3d
JC LanYanFock <JC.LanYanFock@kratosdefense.com>
2022-04-21 18:28:16 +0200

    A

--- Screeny.py
+++ Screeny.py
@@ -3,6 +3,9 @@
 
 from TP_lib import gt1151
 from TP_lib import epd2in13_V2
+from PIL import Image,ImageDraw,ImageFont
+
+fontdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'fonts')
 
 epd = epd2in13_V2.EPD_2IN13_V2()
 gt = gt1151.GT1151()
@@ -13,6 +16,11 @@ def clear_screen():
     gt.GT_Init()
     epd.Clear(0xFF)
 
+def draw_text():
+    font15 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 15)
+    font24 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 24)
+    canvas = Image.new('RGB',(122,250), "orange")
+
 def sleep_screen():
     epd.sleep()
 
@@ -20,4 +28,5 @@ def stop_screen():
     epd.Dev_exit()
 
 clear_screen()
+draw_text()
 sleep_screen()
