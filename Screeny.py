038e4eb8290a426ed3f2af205b78799e68865ed7
JC LanYanFock <JC.LanYanFock@kratosdefense.com>
2022-04-21 18:45:48 +0200

    A

--- Screeny.py
+++ Screeny.py
@@ -10,36 +10,18 @@ import time
 
 fontdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'fonts')
 print(fontdir)
+
 epd = epd2in13_V2.EPD_2IN13_V2()
 gt = gt1151.GT1151()
 GT_Dev = gt1151.GT_Development()
 GT_Old = gt1151.GT_Development()
 
-
+print("init and Clear")
 epd.init(epd.FULL_UPDATE)
 gt.GT_Init()
+epd.Clear(0xFF)
 
-def clear_screen():
-    print("init and Clear")
-    # epd.init(epd.FULL_UPDATE)
-    # gt.GT_Init()
-    #epd.Clear(0xFF)
-
-def draw_text():
-    print((epd.width, epd.height))
-    image = Image.new('1', (epd.width, epd.height), 255)
-    draw = ImageDraw.Draw(image)
-    draw.rectangle((0, 10, 200, 34), fill = 0)
-    epd.init(epd.PART_UPDATE)
-
-def sleep_screen():
-    epd.sleep()
-
-def stop_screen():
-    epd.Dev_exit()
-
-clear_screen()
-while True:
-    draw_text()
-    time.sleep(1)
-#sleep_screen()
+image = Image.new('1', (epd.width, epd.height), 255)
+epd.displayPartBaseImage(epd.getbuffer(image))
+DrawImage = ImageDraw.Draw(image)
+epd.init(epd.PART_UPDATE)
